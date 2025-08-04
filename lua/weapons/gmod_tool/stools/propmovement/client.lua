00e00da2bd204cffba7235bab9ae8c9ac0c3c044
AlanPessaj <alanpessaj123@gmail.com>
2025-08-04 01:06:24 -0300

    Add language strings and rename tool variable
    Added language strings for the PropMovement tool in the client file to improve localization. Renamed the global variable from MOVEMENTTOOL to PROPMOVEMENTTOOL in the shared file for consistency.

--- lua/weapons/gmod_tool/stools/propmovement/client.lua
+++ lua/weapons/gmod_tool/stools/propmovement/client.lua
@@ -1,1 +1,6 @@
 include("shared.lua")
+
+-- Language strings
+language.Add("tool.propmovement.name", "[CGO] PropMovement Tool")
+language.Add("tool.propmovement.desc", "Creado por AlanPessaj ◢ ◤")
+language.Add("tool.propmovement.0", "Configura las opciones en el menú de la herramienta.")
